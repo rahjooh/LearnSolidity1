78b7a008804dd73f228f07682be47d3d7795404f
محمد هادی رهجو <mh.rahjoo@Ab.net>
2018-05-24 18:40:38 +0430

    variables of solidity

---
+++ 0_Variables.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+
+contract Variables {
+    string public one = 'one';
+    int public two = -2;
+    uint public three = 3;  
+    uint256  public four = 4;
+    address  public myAddress = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    bool  public mybool = true ;
+}
